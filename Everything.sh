9d884292f3392d63dd4746fa59dce8046f9ee87c
MJ Roland <chesskingmj@gmail.com>
2023-04-01 19:15:12 -0700

    Initial Commit

---
+++ Everything.sh
@@ -0,0 +1,15 @@
+echo "Starting the installer"
+
+sudo pacman -Syu
+
+
+sudo pacman -S gcc 
+sudo pacman -S python3 
+sudo pacman -S nautilus
+sudo pacman -S gnome-terminal
+
+
+
+
+
+echo "Done With installation"
