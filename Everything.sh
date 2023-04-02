1ebb334dde8bf16eb04fba6f1f7587a26372df55
MJ Roland <chesskingmj@gmail.com>
2023-04-01 19:32:38 -0700

    Added expressvpn

--- Everything.sh
+++ Everything.sh
@@ -5,6 +5,7 @@ sudo pacman -Syu
 
 sudo pacman -S gcc 
 sudo pacman -S python3 
+sudo pacman -S firefox
 sudo pacman -S nautilus
 sudo pacman -S gnome-terminal
 
@@ -14,6 +15,16 @@ echo "Starting git setup"
 git config --global user.email "chesskingmj@gmail.com"
 git config --global user.name "MJ Roland"
 
+echo "Finished git setup"
+
+echo "start expressvpn"
+
+curl --output expressvpn.pkg.tar.xz \
+"https://www.expressvpn.works/clients/linux/expressvpn-3.46.0.7-1-x86_64.pkg.tar.xz"
+
+sudo pacman -U expressvpn.pkg.tar.xz
+
+expressvpn activate
 
 
 
